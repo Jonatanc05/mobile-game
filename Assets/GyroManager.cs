5cb4314cb5d07a0db6cc6d14c0eff7e973a9b3be
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-07-14 13:47:37 -0300

    Criar primeiro protótipo

---
+++ Assets/GyroManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroManager : MonoBehaviour {
+
+    public UnityEngine.UI.Text values;
+    public Transform arrow;
+    public float gravMultiplier;
+
+    private Gyroscope gyro;
+    private bool gyroActive;
+	private bool floating;
+	
+	void Start() {
+		EnableGyro();
+		floating = false;
+	}
+
+    public void EnableGyro() {
+        if (gyroActive)
+            return;
+
+        if (SystemInfo.supportsGyroscope) {
+            gyro = Input.gyro;
+            gyroActive = gyro.enabled = true;
+        }
+		
+        if (!gyroActive)
+		values.text = "Giroscópio não suportado";
+
+    }
+
+    private void Update() {
+		if (!gyroActive)
+			return;
+		
+		Quaternion rot = gyro.attitude;
+        Vector2 down = new Vector2(gyro.gravity.x, gyro.gravity.y).normalized * gravMultiplier;
+
+        if (Mathf.Abs(rot.x) < 0.15f && Mathf.Abs(rot.y) < 0.15f) {
+			values.text = "Dispositivo em uma superfície plana.\nLevante o dispositivo.";
+            arrow.rotation = Quaternion.Euler(0f, 0f, VectorToAngle(down) + 90f);
+            return;
+		}
+        values.text = "";
+
+        Physics2D.gravity = down;
+    }
+
+    private float VectorToAngle(Vector2 v) {
+        return Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+    }
+
+    public Quaternion GetRotation() => gyro.attitude;
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private static readonly float EXTRA_HEIGHT = 0.2f;
+
+	public Joystick joystick;
+	[Range(0, 1)] public float joystickDeadzone;
+	public float maxSpeed;
+	public float jumpForce;
+	public float jumpRequestDuration;
+
+	private Rigidbody2D rb;
+	private BoxCollider2D playerCollider;
+	private LayerMask groundLayerIndex;
+	private float jumpReqRemainingTime;
+    //private float xVel;
+    private Vector2 appliedVelocity;
+    private bool jumpRequest;
+    private Vector2 right;
+    private 
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		playerCollider = GetComponent<BoxCollider2D>();
+		groundLayerIndex = LayerMask.NameToLayer("level");
+    }
+
+	void Update() {
+        /*if (Mathf.Abs(joystick.Horizontal) > joystickDeadzone) {
+			xVel = joystick.Horizontal * maxSpeed;
+		} else {
+			xVel = 0f;
+		}
+
+		if (jumpReqRemainingTime > 0f) {
+			jumpReqRemainingTime -= Time.deltaTime;
+		} else {
+			jumpRequest = false;
+		}
+
+		if (!jumpRequest && joystick.Vertical > .7f) {
+			jumpRequest = true;
+			jumpReqRemainingTime = jumpRequestDuration;
+		}*/
+        right = new Vector2(-Physics2D.gravity.y, Physics2D.gravity.x).normalized;
+        Vector2 j = joystick.Direction;
+        if (j != Vector2.zero) {
+            appliedVelocity = right * maxSpeed * Mathf.Cos((Mathf.Atan2(j.y, j.x)*Mathf.Rad2Deg - Mathf.Atan2(right.y, right.x)*Mathf.Rad2Deg)*Mathf.Deg2Rad);
+        } else
+            appliedVelocity = Vector2.zero;
+        //Debug.Log(Mathf.Cos((Mathf.Atan2(j.y, j.x) * Mathf.Rad2Deg - Mathf.Atan2(right.y, right.x) * Mathf.Rad2Deg)*Mathf.Deg2Rad));
+        //Debug.Log(Mathf.Atan2(j.y, j.x) * Mathf.Rad2Deg - Mathf.Atan2(right.y, right.x) * Mathf.Rad2Deg);
+    }
+
+    void FixedUpdate() {
+        /*if (jumpRequest && IsGrounded()) {
+			rb.velocity += new Vector2(xVel * Time.deltaTime, jumpForce);
+			jumpRequest = false;
+		} else {
+			rb.velocity += new Vector2(xVel * Time.deltaTime, 0f);
+		}*/
+        rb.velocity += appliedVelocity * Time.fixedDeltaTime;
+	}
+
+	public bool IsGrounded() {
+		Collider2D collider = Physics2D.OverlapArea(playerCollider.bounds.min + new Vector3(0.1f, 0f, 0f),
+				new Vector2(playerCollider.bounds.max.x - 0.1f, playerCollider.bounds.min.y - EXTRA_HEIGHT), 1 << groundLayerIndex);
+
+		return collider != null;
+	}
+
+}
