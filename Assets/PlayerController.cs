053c237d0689058e6f845894b1eced6fc359e5f1
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-07-14 18:00:41 -0300

    Implementar pulo que se adapta à orientação

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,72 +4,90 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private static readonly float EXTRA_HEIGHT = 0.2f;
-
 	public Joystick joystick;
-	[Range(0, 1)] public float joystickDeadzone;
+	//[Range(0, 1)] public float joystickDeadzone;
 	public float maxSpeed;
 	public float jumpForce;
 	public float jumpRequestDuration;
+    public float jumpCooldown;
 
-	private Rigidbody2D rb;
+
+    private Rigidbody2D rb;
 	private BoxCollider2D playerCollider;
-	private LayerMask groundLayerIndex;
+	//private LayerMask groundLayerIndex;
 	private float jumpReqRemainingTime;
-    //private float xVel;
+    private float jumpReqRemainingCooldown;
     private Vector2 appliedVelocity;
-    private bool jumpRequest;
-    private Vector2 right;
-    private 
+	private bool jumpRequest;
+	private Vector2 right;
+	private Vector2 up;
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
 		playerCollider = GetComponent<BoxCollider2D>();
-		groundLayerIndex = LayerMask.NameToLayer("level");
-    }
+		//groundLayerIndex = LayerMask.NameToLayer("level");
+		up = Vector2.up;
+		right = Vector2.right;
+	}
 
 	void Update() {
-        /*if (Mathf.Abs(joystick.Horizontal) > joystickDeadzone) {
-			xVel = joystick.Horizontal * maxSpeed;
-		} else {
-			xVel = 0f;
-		}
+		// Gira o vetor da gravidade normalizado 90º sentido anti-horário retornando o que é "direita" na orientação atual
+		right = new Vector2(-Physics2D.gravity.y, Physics2D.gravity.x).normalized;
+		up = new Vector2(-right.y, right.x);
 
-		if (jumpReqRemainingTime > 0f) {
-			jumpReqRemainingTime -= Time.deltaTime;
-		} else {
-			jumpRequest = false;
-		}
+		// Cálculo do quanto mover o player apenas na direção do vetor "right"
+		if (joystick.Direction != Vector2.zero)
+			appliedVelocity = ProjectionOfAOnB(joystick.Direction, right) * maxSpeed;
+		else
+			appliedVelocity = Vector2.zero;
 
-		if (!jumpRequest && joystick.Vertical > .7f) {
+		if (jumpReqRemainingTime > 0f) jumpReqRemainingTime -= Time.deltaTime;
+		else jumpRequest = false;
+        if (jumpReqRemainingCooldown > 0f) jumpReqRemainingCooldown -= Time.deltaTime;
+
+		// Checa se o joystick está para "cima" baseando-se na orientação atual
+		if (!jumpRequest && jumpReqRemainingCooldown <= 0f && ProjectionOfAOnBScale(joystick.Direction, up) > .7f) {
 			jumpRequest = true;
 			jumpReqRemainingTime = jumpRequestDuration;
-		}*/
-        right = new Vector2(-Physics2D.gravity.y, Physics2D.gravity.x).normalized;
-        Vector2 j = joystick.Direction;
-        if (j != Vector2.zero) {
-            appliedVelocity = right * maxSpeed * Mathf.Cos((Mathf.Atan2(j.y, j.x)*Mathf.Rad2Deg - Mathf.Atan2(right.y, right.x)*Mathf.Rad2Deg)*Mathf.Deg2Rad);
-        } else
-            appliedVelocity = Vector2.zero;
-        //Debug.Log(Mathf.Cos((Mathf.Atan2(j.y, j.x) * Mathf.Rad2Deg - Mathf.Atan2(right.y, right.x) * Mathf.Rad2Deg)*Mathf.Deg2Rad));
-        //Debug.Log(Mathf.Atan2(j.y, j.x) * Mathf.Rad2Deg - Mathf.Atan2(right.y, right.x) * Mathf.Rad2Deg);
-    }
+		}
+	}
 
-    void FixedUpdate() {
-        /*if (jumpRequest && IsGrounded()) {
-			rb.velocity += new Vector2(xVel * Time.deltaTime, jumpForce);
-			jumpRequest = false;
-		} else {
-			rb.velocity += new Vector2(xVel * Time.deltaTime, 0f);
-		}*/
-        rb.velocity += appliedVelocity * Time.fixedDeltaTime;
+	void FixedUpdate() {
+		if (jumpRequest && IsGrounded()) {
+            jumpRequest = false;
+            jumpReqRemainingCooldown = jumpCooldown;
+            Debug.Log("pulô");
+			rb.velocity += up * jumpForce;
+		}
+		rb.velocity += appliedVelocity * Time.fixedDeltaTime;
 	}
 
 	public bool IsGrounded() {
-		Collider2D collider = Physics2D.OverlapArea(playerCollider.bounds.min + new Vector3(0.1f, 0f, 0f),
-				new Vector2(playerCollider.bounds.max.x - 0.1f, playerCollider.bounds.min.y - EXTRA_HEIGHT), 1 << groundLayerIndex);
+		ContactPoint2D[] contacts = new ContactPoint2D[8];
+		playerCollider.GetContacts(contacts);
+		foreach (ContactPoint2D contact in contacts) {
+			if (ProjectionOfAOnBScale(contact.normal, up) > 0.6f)
+				return true;
+		}
+		return false;
+	}
 
-		return collider != null;
+	Vector2 ProjectionOfAOnB(Vector2 a, Vector2 b) {
+		if (b == Vector2.zero)
+			return Vector2.zero;
+
+		// Esta é a fórmula que encontrei, funciona mas (acho que) é computacionalmente caro
+		//return b * Mathf.Cos(Mathf.Atan2(a.y, a.x) - Mathf.Atan2(b.y, b.x));
+
+		// Esta é a fórmula matemática de projeção de vetores
+		return b * ( (a.x*b.x + a.y*b.y) / b.sqrMagnitude );
+	}
+
+	float ProjectionOfAOnBScale(Vector2 a, Vector2 b) {
+		if (b == Vector2.zero)
+			return 0f;
+
+		return (a.x * b.x + a.y * b.y) / b.sqrMagnitude;
 	}
 
 }
