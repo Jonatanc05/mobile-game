19f45dfb4f8b865c536f8f3212ee7fdf2584979e
Jônata <Jonatanc0511@gmail.com>
2021-07-14 11:38:39 -0300

    Pequeno polimento

---
+++ Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TargetBehaviour : MonoBehaviour {
+
+	public UnityEngine.UI.Text screenLog;
+
+	public void OnTriggerEnter2D(Collider2D col) {
+		screenLog.text = "boa";
+	}
+
+}
