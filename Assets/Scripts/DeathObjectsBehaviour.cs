1f57751b7fee8c1574c3aeca5c4fbe9e3c34e17d
Jônata <Jonatanc0511@gmail.com>
2021-07-17 18:23:13 -0300

    Criar plataforma fatal

---
+++ Assets/Scripts/DeathObjectsBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathObjectsBehaviour : MonoBehaviour {
+	public GameObject player;
+	public UnityEngine.UI.Text screenLog;
+	public Vector2 playerSpawn;
+
+	public void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject == player) {
+			player.transform.position = playerSpawn;
+			player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			screenLog.text = "ja morreu ze kkkkkk";
+		}
+	}
+
+}
